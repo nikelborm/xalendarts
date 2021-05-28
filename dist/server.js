e3f49ed6f121deebba782f209c97f33c428c4427
sdyakov9 <s.dyakov9@gmail.com>
2021-05-28 23:12:24 +0300

    prepare to push

--- dist/server.js
+++ dist/server.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -25,16 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.initialize = void 0;
 const express_1 = __importDefault(require("express"));
 const router_1 = require("./routes/router");
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const swaggerDoc = __importStar(require("./swagger.json"));
 function initialize() {
     const server = express_1.default();
-    const PORT = process.env.PORT || 3000;
     server.use(express_1.default.json());
-    server.listen(PORT, () => {
+    server.listen(process.env.PORT, () => {
         console.log('Server has been started');
     });
-    server.use('/docs', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDoc));
     server.use(router_1.router);
     return server;
 }
