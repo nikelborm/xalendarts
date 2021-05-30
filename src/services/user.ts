5076aa88fbb7416f5085df038680dc773c8ef8fc
sdyakov9 <s.dyakov9@gmail.com>
2021-05-31 00:36:52 +0300

    docs added

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,7 +2,7 @@ import { User } from '../db/models/user';
 
 export async function userInfo(username: string) {
   const user = await User.findOne({
-    attributes: ['user_id', 'type'],
+    attributes: ['user_id', 'type', 'full_name'],
     where: {
       full_name: username,
     },
