1ca01ca3311d8904f0173f070513386a5816a0f8
nikelborm <kolya007.klass@gmail.com>
2021-05-30 14:35:03 +0300

    docs added

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -6,48 +6,6 @@ import { updateEvent, setEvent, getEvents, deleteEvent } from './event';
 import { getMeetings } from './meeting';
 
 const router = express.Router();
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       required:
- *         - fullName
- *       properties:
- *         user_id:
- *           type: number
- *           description: user id on a system.
- *         type:
- *           type: string
- *           description: user type (student or teacher)
- *         full_name:
- *           type: string
- *           description: Full name of user.
- *       example:
- *         id: 228228
- *         type: student
- *         full_name: Иванов Иван Иванович
- *
- */
-
-/**
- * @swagger
- * /user:
- *  get:
- *    summary: Returns user_id and type
- *    responses:
- *      200:
- *        description: The info of the user
- *        content:
- *          application/json:
- *            schema:
- *              type: array
- *              items:
- *                $ref: '#/components/schemas/User'
- */
-
 router.use(express.static(__dirname + '/templates'));
 
 router.get('/', function (req, res) {
