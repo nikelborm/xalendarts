95bbd149efb3d482acb84c1cdbfd386874659c0a
nikel <r0075301net.r@yandex.ru>
2021-06-07 22:46:32 +0300

    Update connection.ts
    Я такое не прощаю
    Я за такое в жопу нахуй ебу
    
    НЕЛЬЗЯ ВЫКЛАДЫВАТЬ АВТОРИЗАЦИОННЫЕ ДАННЫЕ НА ГИТХАБ
    НЕЛЬЗЯ ВЫКЛАДЫВАТЬ АВТОРИЗАЦИОННЫЕ ДАННЫЕ В ПУБЛИЧНЫЙ ДОСТУП
    И ТЕМ БОЛЕЕ НЕЛЬЗЯ НАХУЙ ДЕЛАТЬ ЭТО ВСЁ ОДНОВРЕМЕННО
    
    Ищи свою БД в прошмандовках азербайджана или меняй пароль
    
    Пароль тебе поменять придётся в любом случае
    
    Скажи спасибо, что это не хакер открыл репо продакшна, а Коля открыл репо учебного проекта
    БД я вам поганить не стал
    
    На этот раз без штрафа, но впредь будьте аккуратнее
    И научись работать либо c dotenv библиотекой, либо c переменными окружения, либо с параметрами запуска процесса ноды или deno, что у вас там
    и если это хероку, то там в настройках  я точно видел кнопку reveal config vars. туда они и прописываются
    а в процессе читаются как показано

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(
-  `postgres://giulymlilntnze:04841c3263c094bd0466037b9161ec19bd8fc88968af9d2f0ce570f0dc492956@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d3jdeo06r3s07h?sslmode=require`,
+  process.env.FUCKING_POSTGRES_CREDENTIALS,
   {
     dialect: 'postgres',
     logging: false,
